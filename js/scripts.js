a36d93d61636a0b14a90f44294dbec46d3fff268
Andrew Accuardi and Bill Klinkow <andrew@>
2016-10-24 21:17:50 -0700

    Creates HTML framework, and the beginnings of JS, and some css styling.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// Business logic
+
+function pigDice (playerOne, playerTwo) {
+this.roll = Math.floor(Math.random() * 10014) +1;
+this.currentPlayer = playerOne;
+this.currentScore = 0;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+// User interface logic
+$().ready()
